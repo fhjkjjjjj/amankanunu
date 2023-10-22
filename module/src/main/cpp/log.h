266a4797b2762c3a393612603c899813d78142fc
Aman Mondal <111458684+fhjkjjjjj@users.noreply.github.com>
2023-10-23 00:00:07 +0530

    Update log.h

--- module/src/main/cpp/log.h
+++ module/src/main/cpp/log.h
@@ -1,12 +1,25 @@
 #ifndef ZYGISK_IMGUI_MODMENU_LOG_H
 #define ZYGISK_IMGUI_MODMENU_LOG_H
 
+//
+//  Logger.h
+//
+//  Created by MJ (Ruit) on 1/1/19.
+//
+#include <jni.h>
 #include <android/log.h>
 
-#define LOG_TAG "Askan"
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+enum LogType {
+    oDEBUG = 3,
+    oERROR = 6,
+    oINFO  = 4,
+    oWARN  = 5
+};
 
+#define TAG OBFUSCATE("Mod_Menu")
+
+#define LOGD(...) ((void)__android_log_print(oDEBUG, TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(oERROR, TAG, __VA_ARGS__))
+#define LOGI(...) ((void)__android_log_print(oINFO,  TAG, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(oWARN,  TAG, __VA_ARGS__))
 #endif //ZYGISK_IMGUI_MODMENU_LOG_H
