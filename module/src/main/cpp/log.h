a6a0cc0755419b680f935b02555d70fb3f50a933
Aman Mondal <111458684+fhjkjjjjj@users.noreply.github.com>
2023-10-23 01:15:34 +0530

    Update log.h

--- module/src/main/cpp/log.h
+++ module/src/main/cpp/log.h
@@ -5,9 +5,9 @@
 
 #define LOG_TAG "Askan"
 
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) ((void)__android_log_print(daDEBUG, TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(daERROR, TAG, __VA_ARGS__))
+#define LOGI(...) ((void)__android_log_print(daINFO,  TAG, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(daWARN,  TAG, __VA_ARGS__))
 
 #endif //ZYGISK_IMGUI_MODMENU_LOG_H
