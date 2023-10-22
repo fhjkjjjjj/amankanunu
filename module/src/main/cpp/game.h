2a93eab140f27979d5a84c2042489fb4893073d9
Aman Mondal <111458684+fhjkjjjjj@users.noreply.github.com>
2023-10-23 01:46:53 +0530

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "zombie.survival.online.craft"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
